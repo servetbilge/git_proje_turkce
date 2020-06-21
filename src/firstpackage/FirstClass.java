9f21f4edf4a3e5be4c9438b1968d8f7e31ba5ec4
servetbilge <servetbilgebay@gmail.com>
2020-06-21 20:04:08 +0200

    üçüncü ekleme

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -5,7 +5,8 @@ public class FirstClass {
 	public static void main(String[] args) {
 		
 		System.out.println("First Commit From Local repository");
-		System.out.println("Second commend from local repository");
+		System.out.println("Second commit from local repository");
+		System.out.println("Third commit from local repository");
 
 	}
 
